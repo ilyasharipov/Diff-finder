a35620a981e417bf89683af1cb7870c2c4ee65cb
1_Hours <sharipov.yanepenb@gmail.com>
2019-07-29 15:20:31 +0700

    add test for recursive files

--- tests/differTest.php
+++ tests/differTest.php
@@ -9,17 +9,33 @@ class differTest extends TestCase
 {
     public function testGetDiffJson()
     {
-        $pathToFirstFile = 'tests/files/before.json';
-        $pathToSecondFile = 'tests/files/after.json';
-        $expected = file_get_contents('tests/files/expected');
+        $pathToFirstFile = 'tests/files/flat/before.json';
+        $pathToSecondFile = 'tests/files/flat/after.json';
+        $expected = file_get_contents('tests/files/flat/expected');
         $this->assertEquals($expected, genDiff($pathToFirstFile, $pathToSecondFile));
     }
 
     public function testGetDiffYml()
     {
-        $pathToFirstFile = 'tests/files/before.yml';
-        $pathToSecondFile = 'tests/files/after.yml';
-        $expected = file_get_contents('tests/files/expected');
+        $pathToFirstFile = 'tests/files/flat/before.yml';
+        $pathToSecondFile = 'tests/files/flat/after.yml';
+        $expected = file_get_contents('tests/files/flat/expected');
+        $this->assertEquals($expected, genDiff($pathToFirstFile, $pathToSecondFile));
+    }
+
+    public function testGetDiffRecursJson()
+    {
+        $pathToFirstFile = 'tests/files/recursive/before.json';
+        $pathToSecondFile = 'tests/files/recursive/after.json';
+        $expected = file_get_contents('tests/files/recursive/expected');
+        $this->assertEquals($expected, genDiff($pathToFirstFile, $pathToSecondFile));
+    }
+
+    public function testGetDiffRecursYml()
+    {
+        $pathToFirstFile = 'tests/files/recursive/before.yml';
+        $pathToSecondFile = 'tests/files/recursive/after.yml';
+        $expected = file_get_contents('tests/files/recursive/expected');
         $this->assertEquals($expected, genDiff($pathToFirstFile, $pathToSecondFile));
     }
 }
